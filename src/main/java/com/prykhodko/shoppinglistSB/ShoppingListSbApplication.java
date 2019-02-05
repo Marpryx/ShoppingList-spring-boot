86b11f3ab59b247fb8ab552e0c064a02f5fc4940
Marpryx <pritismar@hotmail.com>
2019-02-05 15:10:31 -0500

    Initial commit

---
+++ src/main/java/com/prykhodko/shoppinglistSB/ShoppingListSbApplication.java
@@ -0,0 +1,14 @@
+package com.prykhodko.shoppinglistSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListSbApplication.class, args);
+	}
+
+}
+
